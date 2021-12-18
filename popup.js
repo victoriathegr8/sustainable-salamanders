5a8a58807f77ee0ed0a7c50452ed6d3f60868ab3
victoriathegr8 <vbnguyen@uw.edu>
2021-12-18 12:57:34 -0800

    fetched json and displayed filtered company

--- popup.js
+++ popup.js
@@ -1,27 +1,45 @@
+let companyData = [];
+
 // Update the relevant fields with the new data.
 const setDOMInfo = info => {
-    document.getElementById('total').textContent = info.total;
-    document.getElementById('inputs').textContent = info.inputs;
-    document.getElementById('buttons').textContent = info.buttons;
+    let selectedCompany = companyData.filter(company => (
+      company.companyName === info.manufacturer
+    ))
+    selectedCompany = selectedCompany[0];
     document.getElementById('manufacturer').textContent = info.manufacturer;
-    let manufacturers = [];
-    let storage = localStorage["manufacturer"];
-    if (storage != undefined) {
-        manufacturers = JSON.parse(localStorage["manufacturer"])
-    };
-    if (manufacturers.length < 1) {
-        let manArr = [];
-        manArr.push((info.manufacturer));
-        localStorage["manufacturer"] = JSON.stringify(manArr);
-    } else {
-        manufacturers.push((info.manufacturer));
-        localStorage["manufacturer"] = JSON.stringify(manufacturers);
-    }
+    document.getElementById('main').textContent = selectedCompany.mainScore;
+    document.getElementById('climate').textContent = selectedCompany.climate;
+    document.getElementById('water').textContent = selectedCompany.water;
+    document.getElementById('forests').textContent = selectedCompany.forests;
+    // let manufacturers = [];
+    // let storage = localStorage["manufacturer"];
+    // if (storage != undefined) {
+    //     manufacturers = JSON.parse(localStorage["manufacturer"])
+    // };
+    // if (manufacturers.length < 1) {
+    //     let manArr = [];
+    //     manArr.push((info.manufacturer));
+    //     localStorage["manufacturer"] = JSON.stringify(manArr);
+    // } else {
+    //     manufacturers.push((info.manufacturer));
+    //     localStorage["manufacturer"] = JSON.stringify(manufacturers);
+    // }
 
 
   };
   // Once the DOM is ready...
   window.addEventListener('DOMContentLoaded', () => {
+    fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      companyData = data;
+    })
+    .catch((err) => {
+      //throw(err);
+    })
+    .finally(() => {
+      //finally something
+    });
     // ...query for the active tab...
     chrome.tabs.query({
       active: true,
